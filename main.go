31224556422166f11622c396658e69769ffd2c7b
Abdul Latif Syafii <a.l.syafii@gmail.com>
2019-11-14 17:28:36 +0700

    Initial commit

---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo"
+)
+
+type User struct {
+	ID   int
+	Name string
+}
+
+func userHandler(c echo.Context) error {
+	// id, _ := strconv.Atoi(c.QueryParam("id"))
+	// user := User{ID: id, Name: "Poby"}
+	// if len(id) <= 3 {
+	// 	return c.JSON(http.StatusNotFound, "tidak ada")
+	// }
+	data := map[string]interface{}{}
+	c.Bind(&data)
+
+	if data["nama"] == "" {
+		return c.String(403, "nama is required")
+	}
+	return c.JSON(http.StatusOK, data)
+}
+
+func main() {
+	e := echo.New()
+
+	e.POST("/user", userHandler)
+	e.Start(":8000")
+}
